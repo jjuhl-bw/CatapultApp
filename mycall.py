4b8b3e8564c414cb90c8c4a8e5c40535ecab460c
jimmyjuhl <jimmyjuhl@gmail.com>
2015-12-29 17:23:22 -0500

    Update mycall.py

--- mycall.py
+++ mycall.py
@@ -2,6 +2,6 @@ from bandwidth_sdk import Client
 Client('u-uw2wmh7xisddivg5ojerb4q','t-7467ab67iqmubklm4q66ety','on3rwhb6ygrwd6sflor3s554nx4ytfvz4bidfpa')
 
 from bandwidth_sdk import Call
-mycall = Call.create('+15622862284','9196494865')
+mycall = Call.create('+15622862284','destination number')
 
 print mycall
